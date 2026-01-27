dc5b9c0eac7e5cfaa5ad247041992cad782f7957
Antoine <antoinebouclier1@gmail.com>
2026-01-27 08:38:21 +0100

    Ex02: In progress.

--- ex02/inc/Base.hpp
+++ ex02/inc/Base.hpp
@@ -4,7 +4,9 @@
 class Base
 {
 	private:
-		
+		Base();
+		Base(Base const &copy);
+		Base	&operator=(Base const &src);
 	public:
 		virtual ~Base();
 		virtual Base	*generate(void);
--- ex02/src/Base.cpp
+++ ex02/src/Base.cpp
@@ -1,5 +1,21 @@
 #include "Base.hpp"
 
+Base::Base()
+{
+}
+
+Base::Base(Base const &src)
+{
+	*this = src;
+}
+
+Base &Base::operator=(Base const &copy)
+{
+	if (this != &copy)
+		return (*this);
+	return (*this);
+}
+
 Base::~Base()
 {
 }
